807a5efbd95b4e075f053f8ba81251f67a3341c7
Taiwo Osunrinde <55808005+osunrinde@users.noreply.github.com>
2022-07-09 23:01:00 -0700

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from PIL import Image
+
+########################
+# Page Title
+########################
+
+image=Image.Open('https://storage.googleapis.com/afs-prod/media/media:94b8a45144234c3e8d495959903df184/2000.jpeg')
+
+st.image(image, use_column_width=True)
+
+st.write("""
+# Breccia Rock Classification
+
+This application helps to classify breccias into their different classes
+
+***
+""")
