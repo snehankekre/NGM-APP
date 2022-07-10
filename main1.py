520de1c8ad76bb7d3f5e40d648f1adc66f198af9
Taiwo Osunrinde <55808005+osunrinde@users.noreply.github.com>
2022-07-09 23:16:34 -0700

    Add files via upload

--- main1.py
+++ main1.py
@@ -5,7 +5,7 @@ from PIL import Image
 # Page Title
 ########################
 
-image=Image.open("gold.2-1.jpg")
+image=Image.open('https://storage.googleapis.com/afs-prod/media/media:94b8a45144234c3e8d495959903df184/2000.jpeg')
 
 st.image(image, use_column_width=True)
 
