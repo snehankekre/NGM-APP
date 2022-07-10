b1da5d7cf34513a300751617a1f47736059b3132
Taiwo Osunrinde <55808005+osunrinde@users.noreply.github.com>
2022-07-09 23:13:44 -0700

    Add files via upload

--- main1.py
+++ main1.py
@@ -1,4 +1,3 @@
-import altair as alt
 import streamlit as st
 from PIL import Image
 
@@ -6,7 +5,7 @@ from PIL import Image
 # Page Title
 ########################
 
-image=Image.Open('https://storage.googleapis.com/afs-prod/media/media:94b8a45144234c3e8d495959903df184/2000.jpeg')
+image=Image.Open("gold.2-1.jpg")
 
 st.image(image, use_column_width=True)
 
